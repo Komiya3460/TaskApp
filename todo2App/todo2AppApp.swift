ba496b22a07f0e7ff0aea939666eee9d92790159
komiya3460 <ochi130325@gmail.com>
2024-08-13 22:25:46 +0900

    Initial Commit

---
+++ todo2App/todo2AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  todo2AppApp.swift
+//  todo2App
+//
+//  Created by 越智三四郎 on 2024/08/13.
+//
+
+import SwiftUI
+
+@main
+struct todo2AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
